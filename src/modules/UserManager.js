76fa6579cdfbdb7fe58f567074ece9d6ddf33457
Schatfield <dgoldmine@icloud.com>
2021-02-28 14:31:16 -0600

    updated remoteURL link to heroku host

--- src/modules/UserManager.js
+++ src/modules/UserManager.js
@@ -1,29 +1,29 @@
-const remoteURL = "http://localhost:5002"
+const remoteURL = "https://broomsticks-json.herokuapp.com";
 
 export default {
   getUserByEmail(email) {
-    return fetch(`${remoteURL}/users?email=${email}`)
-    .then(result => result.json())
+    return fetch(`${remoteURL}/users?email=${email}`).then((result) =>
+      result.json()
+    );
   },
-  
+
   getUserById(id) {
-    return fetch(`${remoteURL}/users?email=${id}`)
-    .then(result => result.json())
+    return fetch(`${remoteURL}/users?email=${id}`).then((result) =>
+      result.json()
+    );
   },
 
   postNewUser(newUser) {
     return fetch(`${remoteURL}/users`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(newUser)
-    }).then(result => result.json())
+      body: JSON.stringify(newUser),
+    }).then((result) => result.json());
   },
 
-  searchUser(email){
-    return fetch(`${remoteURL}/users?q=${email}`)
-    .then(data => data.json());
-  }
-
+  searchUser(email) {
+    return fetch(`${remoteURL}/users?q=${email}`).then((data) => data.json());
+  },
 };

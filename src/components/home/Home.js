c10bf02ffeffc0f9a063aa5f345b53f94ad0da86
Schatfield <dgoldmine@icloud.com>
2021-03-06 15:14:26 -0600

    flexed welcome images on home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
-import './Home.css'
+import React, { Component } from "react";
+import "./Home.css";
 
 class Home extends Component {
-
   render() {
-
     return (
       <>
-        <div id="logo">
-          <img src={require('./hero-doubleLine.png')} alt="threeBroomsticks" />
+        <div id="hero">
+          <img className="hero-image" src={require("./hero-doubleLine.png")} alt="threeBroomsticks" />
         </div>
         <div className="home-flex-container">
-            
-              <img className="swept-away-flex-item" src={require('./sweptAway.gif')} alt="" />
-            
-
-            <div className="welcome-flex-item">
-            <img className="welcome" src={require('./welcome.png')} alt="" />
-            
-              
-              </div>
-
+          <div className="welcome-flex-container">
+            <img className="" src={require("./sweptAway.gif")} alt="" />
+            <img className="welcome" src={require("./welcome.png")} alt="" />
           </div>
+        </div>
       </>
-    )
+    );
   }
 }
 
